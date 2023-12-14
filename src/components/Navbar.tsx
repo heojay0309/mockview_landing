7e5ee162d463672bf5305456a50d47637b9748be
Jay Heo <heojay0309@gmail.com>
2023-12-13 21:36:08 -0800

    added href to title

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,10 @@ const Navbar = () => {
   return (
     <header className="text-gray-400 bg-gray-900 body-font fixed w-screen">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+        <a
+          href="#home"
+          className="flex title-font font-medium items-center text-white mb-4 md:mb-0"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
