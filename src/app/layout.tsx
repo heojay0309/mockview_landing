f62e060ad2386fb14805d65a4a02dfe0bc974510
Jay Heo <heojay0309@gmail.com>
2024-04-09 00:04:11 -0700

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
-import Background from '@/constants/Background';
 import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -22,15 +22,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} `}>
-        {/* <div className="scroll-watcher"></div> */}
-        {/* <Background /> */}
-
         <div className="relative z-10 w-full justify-between flex  flex-col bg-gradient-to-b from-[#010E21] via-slate-700 to-[#010E21]">
           <Navbar />
           <div className="flex-1">{children}</div>
           <Footer />
           <Toaster />
         </div>
+        <Analytics />
       </body>
     </html>
   );
